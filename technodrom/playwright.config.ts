5e1029b7ef26b155d68f2f3e01568f22f53adf54
Yulia <99640795+pinkx1@users.noreply.github.com>
2025-03-14 11:29:29 +0400

    Update playwright.config.ts

--- technodrom/playwright.config.ts
+++ technodrom/playwright.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [['list'], ['./reporter.ts']],
   timeout: 180000,
